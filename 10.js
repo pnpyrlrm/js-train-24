ad289f1b1239d0c63c5969bd1f5932dd33b2fac7
pnpyrlrm <dv56026@gmail.com>
2023-11-17 19:48:26 +0200

    end 24 train

--- 10.js
+++ 10.js
@@ -2,11 +2,32 @@ console.log("Завдання: 10 ==============================");
 
 function task10() {
   // Створюємо проміс promise1, які вирішуються через 500мс, та повертають як успішне виконання рядок "Promise 1".
+  const promise1 = new Promise((res) => {
+    setTimeout(() => {
+      res(`Promise 1`);
+    }, 500);
+  });
   // Створюємо проміс promise2, які вирішуються через 200мс, та повертають як успішне виконання рядок "Promise 2".
+  const promise2 = new Promise((res) => {
+    setTimeout(() => {
+      res(`Promise 2`);
+    }, 200);
+  });
   // Створюємо проміс promise3, які вирішуються через 300мс, та повертають як успішне виконання рядок "Promise 3".
+  const promise3 = new Promise((res) => {
+    setTimeout(() => {
+      res(`Promise 3`);
+    }, 300);
+  });
   // Promise.any() приймає масив промісів які ми створили вище і вирішується, як тільки перший із цих промісів вирішується.
+  const any = Promise.any([promise1, promise2, promise3]);
   // Якщо проміс виконаний успішно виводимо в консоль дані які він повертає
   // Якщо проміс виконаний з помилкою виводимо в консоль помилку
+  any
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => console.error(error));
 }
 
 // Викликаємо функцію task10
